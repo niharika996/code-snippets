abdf43ac81a6a5ee1ae4dce0c4a0ab8cdc1e1e14
niharika996 <niharikaxx06@gmail.com>
2025-08-06 14:09:12 +0530

    Create fetchAPI.js

---
+++ fetchAPI.js
@@ -0,0 +1,7 @@
+// fetchAPI.js
+// Fetch data from an API
+
+fetch("https://api.example.com/data")
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error));
